497e72c6f3d8ec947595776d459d015134a96983
Oksana Zakharevych <zakharevych.oksana@gmail.com>
2026-06-04 15:04:39 +0200

    Solution

--- src/components/FindMovie/FindMovie.tsx
+++ src/components/FindMovie/FindMovie.tsx
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import './FindMovie.scss';
+import classNames from 'classnames';
+import { Movie } from '../../types/Movie';
+import { MovieCard } from '../MovieCard';
+import { getMovie } from '../../api';
 
-export const FindMovie: React.FC = () => {
+type Props = {
+  movies: Movie[];
+  setMovies: (movies: Movie[]) => void;
+};
+
+export const FindMovie: React.FC<Props> = ({ movies, setMovies }) => {
+  const [query, setQuery] = useState('');
+  const [foundMovie, setFoundMovie] = useState<Movie | null>(null);
+  const [hasFoundError, setHasFoundError] = useState(false);
+  const [hasDuplicateError, setHasDuplicateError] = useState(false);
+  const [isPreviewShown, setIsPreviewShown] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handlerSearchMovie(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const fetchedMovie = await getMovie(query);
+
+      if (fetchedMovie && fetchedMovie.Response !== 'False') {
+        setFoundMovie(fetchedMovie);
+        setIsPreviewShown(true);
+      } else {
+        setHasFoundError(true);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  function handlerChangeQuery(event: ChangeEvent<HTMLInputElement>) {
+    setQuery(event.target.value);
+    setHasFoundError(false);
+  }
+
+  function handleAddMovie() {
+    if (movies.some(currMovie => currMovie.imdbID === foundMovie?.imdbID)) {
+      setQuery('');
+      setIsPreviewShown(false);
+      setFoundMovie(null);
+      setHasDuplicateError(true);
+
+      return;
+    }
+
+    setMovies(prevMovies => [...prevMovies, foundMovie]);
+    setIsPreviewShown(false);
+    setQuery('');
+  }
+
   return (
     <>
-      <form className="find-movie">
+      <form className="find-movie" onSubmit={handlerSearchMovie}>
         <div className="field">
           <label className="label" htmlFor="movie-title">
             Movie title
@@ -16,13 +70,23 @@ export const FindMovie: React.FC = () => {
               type="text"
               id="movie-title"
               placeholder="Enter a title to search"
-              className="input is-danger"
+              className={classNames('input', { isDanger: hasFoundError })}
+              value={query}
+              onChange={handlerChangeQuery}
             />
           </div>
 
-          <p className="help is-danger" data-cy="errorMessage">
-            Can&apos;t find a movie with such a title
-          </p>
+          {hasFoundError && (
+            <p className="help is-danger" data-cy="errorMessage">
+              Can&apos;t find a movie with such a title
+            </p>
+          )}
+
+          {hasDuplicateError && (
+            <p className="help is-danger" data-cy="errorMessage">
+              This movie is already in your list
+            </p>
+          )}
         </div>
 
         <div className="field is-grouped">
@@ -30,28 +94,36 @@ export const FindMovie: React.FC = () => {
             <button
               data-cy="searchButton"
               type="submit"
-              className="button is-light"
+              className={classNames('button is-light', {
+                'is-loading': isLoading,
+              })}
+              disabled={!query}
             >
-              Find a movie
+              {isPreviewShown ? 'Search again' : 'Find a movie'}
             </button>
           </div>
 
-          <div className="control">
-            <button
-              data-cy="addButton"
-              type="button"
-              className="button is-primary"
-            >
-              Add to the list
-            </button>
-          </div>
+          {isPreviewShown && foundMovie && (
+            <div className="control">
+              <button
+                data-cy="addButton"
+                type="button"
+                className="button is-primary"
+                onClick={() => handleAddMovie()}
+              >
+                Add to the list
+              </button>
+            </div>
+          )}
         </div>
       </form>
 
-      <div className="container" data-cy="previewContainer">
-        <h2 className="title">Preview</h2>
-        {/* <MovieCard movie={movie} /> */}
-      </div>
+      {isPreviewShown && foundMovie && (
+        <div className="container" data-cy="previewContainer">
+          <h2 className="title">Preview</h2>
+          <MovieCard movie={foundMovie} />
+        </div>
+      )}
     </>
   );
 };
